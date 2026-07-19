d0fac4ba6d1d2d019add72032160ea5da505a2aa
Fuschy <tim@mayershofer.de>
2026-07-19 15:22:59 +0200

    Add files via upload

---
+++ locg_reading_list.cbl
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList>
+  <Name>COGU - Chapter 0 - Prologue</Name>
+  <Books>
+    <Book Series="Marvels Annotated" Number="1" Volume="2019" />
+    <Book Series="Marvels Annotated" Number="2" Volume="2019" />
+    <Book Series="Marvels Annotated" Number="3" Volume="2019" />
+    <Book Series="Marvels Annotated" Number="4" Volume="2019" />
+    <Book Series="Marvels Epilogue" Number="1" Volume="2019" />
+    <Book Series="Marvels: Eye of the Camera" Number="1" Volume="2008" />
+    <Book Series="Marvels: Eye of the Camera" Number="2" Volume="2008" />
+    <Book Series="Marvels: Eye of the Camera" Number="3" Volume="2008" />
+    <Book Series="Marvels: Eye of the Camera" Number="4" Volume="2008" />
+    <Book Series="Marvels: Eye of the Camera" Number="5" Volume="2008" />
+    <Book Series="Marvels: Eye of the Camera" Number="6" Volume="2008" />
+    <Book Series="History of the Marvel Universe" Number="3" Volume="2019" />
+    <Book Series="History of the Marvel Universe" Number="4" Volume="2019" />
+    <Book Series="The Sentry" Number="1" Volume="2000" />
+    <Book Series="The Sentry" Number="2" Volume="2000" />
+    <Book Series="The Sentry" Number="3" Volume="2000" />
+    <Book Series="The Sentry" Number="4" Volume="2000" />
+    <Book Series="The Sentry" Number="5" Volume="2000" />
+    <Book Series="The Sentry / Fantastic Four" Number="1" Volume="2001" />
+    <Book Series="The Sentry / Hulk" Number="1" Volume="2001" />
+    <Book Series="The Sentry / Spider-Man" Number="1" Volume="2001" />
+    <Book Series="The Sentry / X-Men" Number="1" Volume="2001" />
+    <Book Series="The Sentry vs. the Void" Number="1" Volume="2001" />
+    <Book Series="Detective Comics" Number="1111" Volume="1937" />
+    <Book Series="Absolute Wonder Woman" Number="22" Volume="2024" />
+    <Book Series="Action Comics" Number="1100" Volume="1938" />
+  </Books>
+</ReadingList>
